ad70e64c7b64476bd45857fa4bfd92ab0da29427
sonnguyen89 <nam_son14@yahoo.com>
2018-04-11 10:56:43 +1000

    update product main image content

--- wp-content/themes/Kokoda/single-product.php
+++ wp-content/themes/Kokoda/single-product.php
@@ -54,6 +54,29 @@
 	</div>
 </div>
 
+<div class="mobile-banner-wrap stripe center">
+        <div class="banner container">
+            <div class="row">
+                <div class="banner-content">
+                    <h1><?php the_title(); ?></h1>
+                    <p class="lead"><?php the_field('banner_description'); ?></p>
+
+                    <div class="product-meta">
+                        <?php if(get_field('price_thousands')): ?><span class="price">$<?php the_field('price_thousands'); ?>,<?php the_field('price_hundreds'); ?><i>+ORC</i></span><?php endif; ?>
+                        <?php if(get_field('size_feet')): ?><span class="size"><?php the_field('size_feet'); ?>'<?php if(get_field('size_inches')): ?><?php the_field('size_inches'); ?>"<?php endif; ?></span><?php endif; ?>
+                        <?php if(get_field('occupants')): ?><span class="occupants"><?php the_field('occupants'); ?></span><?php endif; ?>
+                    </div>
+                    <?php if(get_field('brochure_pdf')): ?>
+                        <div class="product-brochure-button">
+                            <a href="<?php the_field('brochure_pdf'); ?>" target="_blank">Download Brochure</a>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+</div>
+
+
 <?php if(get_field('overview_heading') || get_field('overview_text') || get_field('video')):?>
 <div class="stripe center intro">
 	<div class="container">
