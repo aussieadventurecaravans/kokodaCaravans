be153af8315fc9890c1a0d573e0a8d44cc090143
sonnguyen89 <nam_son14@yahoo.com>
2018-05-23 16:39:50 +1000

    update logo for mobile

--- wp-content/themes/Kokoda/header.php
+++ wp-content/themes/Kokoda/header.php
@@ -78,7 +78,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="brand auto-top" href="<?php bloginfo('url') ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/_img/log-blacko.png"></a>
+					<a class="brand auto-top" href="<?php bloginfo('url') ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/_img/logo_black_text.png"></a>
 				</div>
 
 				<div class="collapse navbar-collapse" id="navbar-collapse">
