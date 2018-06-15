58c48279dc3f0e77cd5d30af3c026d1d7968c4eb
sonnguyen89 <nam_son14@yahoo.com>
2018-06-15 14:27:04 +1000

    align the banner content to the logo

--- wp-content/themes/Kokoda/content-home.php
+++ wp-content/themes/Kokoda/content-home.php
@@ -267,5 +267,16 @@
         });
 
 
+        var logo_offset = $('.navbar-default .navbar-header .brand img').offset();
+
+        $('.page.home .banner-wrap .banner .row .banner-content').css({'left' : logo_offset.left});
+        $( window ).resize(function(){
+            if($(window).width() > 776 )
+            {
+                var logo_offset = $('.navbar-default .navbar-header .brand img').offset();
+                $('.page.home .banner-wrap .banner .row .banner-content').css({'left' : logo_offset.left});
+            }
+
+        });
     });
 </script>
