f57c5445200ac6979ab26c66c57b2ade461a4c67
sonnguyen89 <nam_son14@yahoo.com>
2018-11-01 17:28:55 +1100

    update custom orders portal

--- custom-orders-portal/application/config/database.php
+++ custom-orders-portal/application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => DB_HOST,
+	'username' => DB_USER,
+	'password' => DB_PASSWORD,
 	'database' => 'kokoda_custom_order',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
