55ec58ddc78b1cdae602215df12b9924327f6ac1
sonnguyen89 <nam_son14@yahoo.com>
2018-11-13 16:45:13 +1100

    update custom order portal

--- custom-orders-portal/application/controllers/Quote_request.php
+++ custom-orders-portal/application/controllers/Quote_request.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Quote_Request extends CI_Controller {
+class Quote_request extends CI_Controller {
 
     public function __construct()
     {
